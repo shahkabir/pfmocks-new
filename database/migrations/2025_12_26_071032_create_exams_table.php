f6c0b0bbe8ac4ea10e484136ef0be7f10b0dd94f
A. M. M. Shahriar Kabir <ashahriarkabir@banglalink.net>
2026-04-29 21:15:54 +0600

    fix exams migration

--- database/migrations/2025_12_26_071032_create_exams_table.php
+++ database/migrations/2025_12_26_071032_create_exams_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('exams', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // IELTS, PTE
+            $table->string('tag', 45);
             $table->boolean('is_active')->default(true);
             $table->timestamps();
          });
